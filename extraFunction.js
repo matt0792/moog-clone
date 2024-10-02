9787a59f951feb302a27a4ba59f72c491bf58e0d
Matt Thompson <matthew.thompson6@icloud.com>
2024-10-02 14:59:26 +0200

    Initial Commit

---
+++ extraFunction.js
@@ -0,0 +1,36 @@
+// alert(`
+// This website is a clone of the moog homepage, as of October 2024.
+
+// Visit moogmusic.com to view the original`);
+
+document.addEventListener("wheel", function (e) {
+  const scrollContainer = document.querySelector(".horizontal-scroll");
+
+  // translate vertical scroll to horizontal scroll
+  // Got a lot of help from stack overflow to achieve this
+  if (e.deltaY !== 0) {
+    scrollContainer.scrollLeft += e.deltaY;
+    // To prevent vertical scroll
+    e.preventDefault();
+  }
+});
+
+// Sets modal to show when page is loaded 
+window.onload = function() {
+    document.getElementById('creditModal').style.display = 'block';
+};
+
+// Gets relevant items 
+var modal = document.getElementById("creditModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// Closes modal when x, or anywhere else is clicked 
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
